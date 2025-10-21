6b02ff3c2df31cf74ceba8effb405b0513115326
LENOVO <adeliafitriani9@gmail.com>
2025-10-21 08:59:05 +0700

    2 Greeter2

--- src/id/ac/polbeng/adelia/test_kelas/GreeterTest.kt
+++ src/id/ac/polbeng/adelia/test_kelas/GreeterTest.kt
@@ -1,7 +1,12 @@
 package id.ac.polbeng.adelia.test_kelas
 
-
 fun main() {
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
 }
+
